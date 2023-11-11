e492d12060d2290c2d03a31651e252c00755e342
davidpracilio <dave.pracilio@outlook.com>
2023-11-11 20:21:59 +0800

    Add content to home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,6 +4,13 @@ import '../../App.css';
 export default function Home() {
   return (
     <>
+      <section className='banner'>
+        <h1 className='white'>A tool for you.</h1>
+        <h1 className='yellow'>Track your learning.</h1>
+        <p>Keep notes about your learning, get some suggestions or plan a path. 
+          Grow is your learning platform availabile anywhere.
+        </p>
+      </section>
     </>
   );
 }
