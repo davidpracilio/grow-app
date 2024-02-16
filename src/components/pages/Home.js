6d61563cc16e0c9dca98ab20bb91d9e655ad9b06
davidpracilio <dave.pracilio@outlook.com>
2024-02-16 10:31:06 +0800

    Resolve left padding issue on tasks

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -9,7 +9,7 @@ export default function Home() {
         <h1 className='white'>A tool for you.</h1>
         <h1 className='yellow'>Track your learning.</h1>
         <p>Keep notes about your learning, get some suggestions or plan a path. 
-          Grow is your learning platform availabile anywhere.
+          Grow is your learning assistant availabile anywhere.
         </p>
       </section>
     </>
